235bb3004b5d8c2d2abec072f47485a721cb22ea
Mardon <aslonovmardon1995@gmail.com>
2023-05-27 15:48:18 +0500

    First push

---
+++ resources/views/test.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>GRE test</title>
+</head>
+
+<body>
+
+
+    <div class="container mt-5">
+        <div class="d-flex justify-content-center row">
+            <div class="col-md-10 col-lg-10">
+                <div class="border">
+                    <div class="question bg-white p-3 border-bottom">
+                        <div class="d-flex flex-row justify-content-between align-items-center mcq">
+                            <h4>GRE exam - 0001</h4><span>(1 of 100)</span>
+                        </div>
+                    </div>
+                    <div class="question bg-white p-3 border-bottom">
+                        <div class="d-flex flex-row align-items-center question-title">
+                            <h3 class="text-danger">1.</h3>
+                            <h5 class="mt-1 ml-2">Which of the following country has largest population?</h5>
+                        </div>
+                        <div class="ans ml-2">
+                            <label class="radio"> <input type="radio" name="brazil" value="brazil">
+                                <span>Brazil</span>
+                            </label>
+                        </div>
+                        <div class="ans ml-2">
+                            <label class="radio"> <input type="radio" name="Germany" value="Germany">
+                                <span>Germany</span>
+                            </label>
+                        </div>
+                        <div class="ans ml-2">
+                            <label class="radio"> <input type="radio" name="Indonesia" value="Indonesia">
+                                <span>Indonesia</span>
+                            </label>
+                        </div>
+                        <div class="ans ml-2">
+                            <label class="radio"> <input type="radio" name="Russia" value="Russia">
+                                <span>Russia</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between align-items-center p-3 bg-white"><button
+                            class="btn btn-primary d-flex align-items-center btn-danger" type="button"><i
+                                class="fa fa-angle-left mt-1 mr-1"></i>&nbsp;previous</button><button
+                            class="btn btn-primary border-success align-items-center btn-success" type="button">Next<i
+                                class="fa fa-angle-right ml-2"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>GRE Uzbekistan</title>
+
+</head>
+
+<body>
+    <div class="d-flex justify-content-around">
+        <a href="login"><button type="button" class="btn btn-outline-success">Tilloyi ki?</button></a><br>
+        <a href="test"><button type="button" class="btn btn-outline-primary">Molekular
+                fizika</button></a><br>
+        <a href="gre"><button type="button" class="btn btn-outline-secondary">Elektrodinamika</button></a><br>
+        <a href="optika.html"><button type="button" class="btn btn-outline-danger">Optika</button></a><br>
+        <a href="atom.html"><button type="button" class="btn btn-outline-warning">Atom va yadro
+                fizikasi</button></a><br>
+    </div>
+</body>
+
+</html>
