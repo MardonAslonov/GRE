dcd1add1637ba74e026f672e19080abf470c4378
Mardon <aslonovmardon1995@gmail.com>
2023-06-11 16:56:22 +0500

    seedergacha bo'ldi

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $check = User::where('phone',+998998955991)->first();
+        if (empty($check)){
+            $user = new User();
+            $user->name = "Mardon";
+            $user->surname = "Aslonov";
+            $user->phone = "+998998955991";
+            $user->password = bcrypt('2321341');
+            $user->save();
+        }
+    }
+}
