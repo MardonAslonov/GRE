18659444c1610794638232a60876007e9cd637a1
Mardon <aslonovmardon1995@gmail.com>
2023-06-13 09:58:13 +0500

    rasm chiqarishgacha bo'ldi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,24 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Variant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function auth(Request $request){
-        $credentials =[
+    public function auth(Request $request)
+    {
+        $credentials = [
             'name' => $request->name,
             'password' => $request->password
         ];
-        if(Auth::attempt($credentials)){
-            return view('gre');
-        }else{
+        if (Auth::attempt($credentials)) {
+            $variants = Variant::all();
+            return view('gre', [
+                'variants' => $variants,
+            ]);
+            // return view('gre');
+        } else {
             return redirect()->back();
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return view('login');
     }
--- resources/views/gre.blade.php
+++ resources/views/gre.blade.php
@@ -54,164 +54,30 @@
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top"
-                            src="https://0.academia-photos.com/attachment_thumbnails/28148429/mini_magick20180818-7998-xrlql5.png?1534647739"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR1776</h5>
-                                <!-- Product price-->
-                                $00.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto"
-                                    href="{{ route('start', $id = 1) }}">Boshlash
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-success text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
-                            New
-                        </div>
-                        <!-- Product image-->
-                        <img class="card-img-top"
-                            src="https://m.media-amazon.com/images/I/61Yfp1DrH0L._AC_UF1000,1000_QL80_.jpg"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR - 0877</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
+                @foreach ($variants as $variant)
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="{{ asset('storage/variant/' . $variant->nameImage) }}"
+                                alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">{{ $variant->number }}</h5>
+                                    <!-- Product price-->
+                                    $00.00
                                 </div>
-                                <!-- Product price-->
-                                {{-- <span class="text-muted text-decoration-line-through">$20.00</span> --}}
-                                $00.00
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto"
+                                        href="{{ route('start', $id = 1) }}">Boshlash
+                                    </a></div>
                             </div>
                         </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Boshlash
-                                </a></div>
-                        </div>
                     </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-success text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
-                            New</div>
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://m.media-amazon.com/images/I/61OsysiqxTL.jpg"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR - 1776</h5>
-                                <!-- Product price-->
-                                {{-- <span class="text-muted text-decoration-line-through">$50.00</span> --}}
-                                $00.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Boshlash
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://www.physicsgreprep.com/frontcover.png"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR - 1777</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                $00.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Boshlash
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-success text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
-                            New</div>
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://www.iclass-study.ru/images/GRE_Physics.jpg"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR - 9277</h5>
-                                <!-- Product price-->
-                                {{-- <span class="text-muted text-decoration-line-through">$50.00</span> --}}
-                                $00.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Boshlash
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://m.media-amazon.com/images/I/51XCVH3FZQL.jpg"
-                            alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">GR - 9677</h5>
-                                <!-- Product price-->
-                                $00.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Boshlash
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
